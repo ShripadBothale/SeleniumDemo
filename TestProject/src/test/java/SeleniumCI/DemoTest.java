f0e58251f5be64722ca9beff1da9ccf300747f8e
ShripadBothale <shripad.bothale@gmail.com>
2014-12-03 20:43:06 +0530

    test

--- TestProject/src/test/java/SeleniumCI/DemoTest.java
+++ TestProject/src/test/java/SeleniumCI/DemoTest.java
@@ -1,4 +1,4 @@
-package Selenium.CI;
+package SeleniumCI;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -6,14 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DemoTest {
-
+	
 	@Test
-	public void FirstTest (){
-		WebDriver driver =new FirefoxDriver();
-		  String baseUrl = "http://en.wikipedia.org/wiki/Main_Page";
+	public void Democlass (){
+		WebDriver driver = new FirefoxDriver();
+		String baseUrl = "http://en.wikipedia.org/wiki/Main_Page";
 		  driver.get(baseUrl);
 		  driver.findElement(By.xpath(".//*[@id='searchInput']")).sendKeys("software testing");
 		  driver.findElement(By.xpath("//*[@id='searchButton']")).click();
 		  driver.close();
-	}
+}
 }
