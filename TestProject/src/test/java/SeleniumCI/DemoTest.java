ccf7ed9917d9fe1ead70db5c672b1de554084178
ShripadBothale <shripad.bothale@gmail.com>
2014-12-04 21:13:33 +0530

    sauce changes

--- TestProject/src/test/java/SeleniumCI/DemoTest.java
+++ TestProject/src/test/java/SeleniumCI/DemoTest.java
@@ -4,12 +4,22 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.sun.jna.Platform;
 
 public class DemoTest {
 	
 	@Test
 	public void Democlass (){
 		WebDriver driver = new FirefoxDriver();
+//		 DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+//	        capabilities.setCapability("version", "5");
+//	        capabilities.setCapability("platform", Platform.WINDOWS);
+//		String baseUrl = "http://en.wikipedia.org/wiki/Main_Page";
+//		this.driver = new RemoteWebDriver(new URL("http://shripadbothale:661de51c-84ea-4704-b6a0-58db91abd495@ondemand.saucelabs.com:80/wd/hub"),
+//                capabilities);
 		String baseUrl = "http://en.wikipedia.org/wiki/Main_Page";
 		  driver.get(baseUrl);
 		  driver.findElement(By.xpath(".//*[@id='searchInput']")).sendKeys("software testing");
