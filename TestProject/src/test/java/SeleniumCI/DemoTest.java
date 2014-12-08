e441ecf84cc79c18d9ce15707d528034c613bb45
ShripadBothale <shripad.bothale@gmail.com>
2014-12-08 16:02:58 +0530

    removed commenting

--- TestProject/src/test/java/SeleniumCI/DemoTest.java
+++ TestProject/src/test/java/SeleniumCI/DemoTest.java
@@ -10,13 +10,7 @@ public class DemoTest {
 	
 	@Test
 	public void Democlass (){
-		WebDriver driver = new FirefoxDriver();
-//		 DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-//	        capabilities.setCapability("version", "5");
-//	        capabilities.setCapability("platform", Platform.WINDOWS);
-//		String baseUrl = "http://en.wikipedia.org/wiki/Main_Page";
-//		this.driver = new RemoteWebDriver(new URL("http://shripadbothale:661de51c-84ea-4704-b6a0-58db91abd495@ondemand.saucelabs.com:80/wd/hub"),
-//                capabilities);
+	WebDriver driver = new FirefoxDriver();	
 		String baseUrl = "http://en.wikipedia.org/wiki/Main_Page";
 		  driver.get(baseUrl);
 		  driver.findElement(By.xpath(".//*[@id='searchInput']")).sendKeys("software testing");
